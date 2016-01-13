7c51d28203ae65abd2b5d905b4e6feb275037b92
snehalgaikwad <sgaikwad@agilesoftsol.com>
2016-01-13 17:53:37 +0530

    new files added

---
+++ application/controllers/Booking.php
@@ -0,0 +1,7 @@
+<?php
+class Booking extends CI_Controller {
+
+	public function index() {
+		$this->load->view ( 'booking.html' );
+	}
+}
---
+++ application/controllers/Typography.php
@@ -0,0 +1,7 @@
+<?php
+class Typography extends CI_Controller {
+
+	public function index() {
+		$this->load->view ( 'typography.html' );
+	}
+}
