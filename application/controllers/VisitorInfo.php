211c2fd790f53daa5d1f874ab6ebf3e34f47da70
snehalgaikwad <sgaikwad@agilesoftsol.com>
2016-01-14 17:50:09 +0530

    visitorinfo controller

---
+++ application/controllers/VisitorInfo.php
@@ -0,0 +1,27 @@
+<?php
+class VisitorInfo extends CI_Controller {
+	public function index() {
+		$this->load->view ( 'about.html' );
+	}
+	public function insertVisitorData() {
+		
+			
+			$visitorInfo = array (
+					'visitor_ip' => $_POST ['ip'],
+					'city' => $_POST ['city'],
+					'region' => $_POST ['region'],
+					'country' => $_POST ['country'] ,
+					'date_visited'=>date('Y-m-d')
+			);
+			$this->load->model ( 'PropertyModel' );
+			$visitorInfo = $this->PropertyModel->insertVisitorData ( $visitorInfo );
+		
+	}
+	
+	public function getVisitorCount(){
+		$this->load->model ( 'PropertyModel' );
+		$visitorCount = $this->PropertyModel->getVisitorCount ();
+		$response=array('count'=>$visitorCount);
+		echo json_encode($response);
+	}
+}
