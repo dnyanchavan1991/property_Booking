b2799abb6b490d37657fdef64e3323efc7d7d150
shrutikharge <skharge@agilesoftsol.com>
2016-01-15 18:38:05 +0530

    updated index as controllers/Index1.php

--- application/controllers/Index1.php
+++ application/controllers/Index1.php
@@ -4,4 +4,18 @@ class Index1 extends CI_Controller {
 	public function index() {
 		$this->load->view ( 'index.html' );
 	}
+	
+	public  function getlastMinDeal(){
+		$this->load->model ( 'PropertyModel' );
+		$lastMinDealData = $this->PropertyModel->getlastMinDeal ();
+		$i=0;
+	    $list = array();
+		foreach($lastMinDealData as $row)
+		{
+			
+			  $list[] = array('name' =>$row->name, 'desc' => $row->des,'image'=>$row->imagepath);
+		}
+		echo json_encode( $list);
+		
+	}
 }
