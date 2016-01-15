3aafd9ad8bd7bc8b61ddf05676218366f3104104
vishuawate <vishu.awate@gmail.com>
2016-01-15 16:18:30 +0530

    - Webpage content rearrangement - Related controller file

---
+++ js/controller/landingPageController.js
@@ -0,0 +1,7 @@
+	angular.module('landingPageApp', [])
+				 .controller('landingPageCntrl', function($scope, $http) {
+					  $scope.accomodationType = ["Villa", "Dormatory", "Apartment", "Bunglow", "Row House", "Cottage", "Hut", "Homestay", "Heritage","House boat", "Tree house"];
+					  $scope.guestHeadCount = ["1", "2", "3", "4", "5", "6" ,"7", "8", "9", "10", "11", "12", "13", "14", "15"];				 
+
+				});
+
