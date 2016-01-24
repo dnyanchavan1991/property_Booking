e4a5dfdc2e2dddabe1909638fdfb81bd4974783a
vishuawate <vishu.awate@gmail.com>
2016-01-24 22:03:35 +0530

    - Modification in Dropdown selection

--- js/controller/landingPageController.js
+++ js/controller/landingPageController.js
@@ -1,6 +1,28 @@
 	angular.module('landingPageApp', [])
 				 .controller('landingPageCntrl', function($scope, $http) {
-					  $scope.accomodationType = ["All","Villa", "Dormatory", "Apartment", "Bunglow", "Row House", "Cottage", "Hut", "Homestay", "Heritage","House boat", "Tree house"];
+					  $scope.accomodationType =[
+					                             { value: '0',
+					                            	 label: 'All' },
+					                               { value: '1',
+						                               label: 'Villa' },
+						                           { value: '2',
+							                           label: 'Dormatory' },
+						                           { value: '3',
+							                            label: 'Apartment' },
+						                           { value: '4',
+							                            label: 'Bunglow' },
+						                           { value: '5',
+							                            label: 'Row House' },
+						                           { value: '6',
+							                            label: 'Cottage' },
+						                           { value: '7',
+							                            label: 'Hut' },
+						                           { value : '8',
+							                             label : 'House boat' },
+					                               { value : '9',
+							                             label : 'Tree house' }						                               
+					                               ];
+					  
 					  $scope.guestHeadCount = ["1", "2", "3", "4", "5", "6" ,"7", "8", "9", "10", "11", "12", "13", "14", "15"];
 					  $scope.inputDestination = "Where do you want to go?";
 					  $scope.expandFilterOptions = function(){
