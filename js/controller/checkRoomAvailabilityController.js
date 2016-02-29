65177bcfd5b967bf8889d96500e6ea8a993cafc4
snehalgaikwad <sgaikwad@agilesoftsol.com>
2016-02-29 12:56:57 +0530

    removed pagination code from checkRoomAvailabilityController.js

--- js/controller/checkRoomAvailabilityController.js
+++ js/controller/checkRoomAvailabilityController.js
@@ -1,5 +1,5 @@
 angular
-		.module('checkRoomAvailabilityApp', ['ui.bootstrap'])
+		.module('checkRoomAvailabilityApp', [])
 		.controller(
 				'checkRoomAvailabilityController',
 				function($scope, $http) {
@@ -159,24 +159,6 @@ angular
 						objForm.submit();
 
 					}
-					$scope.filteredTodos = []
-					  ,$scope.currentPage = 1
-					  ,$scope.numPerPage = 10
-					  ,$scope.maxSize = 5;
-					 
-					  $scope.makeTodos = function() {
-					    $scope.todos = [];
-					    for (i=1;i<=1000;i++) {
-					      $scope.todos.push({ text:'todo '+i, done:false});
-					    }
-					  };
-					  $scope.makeTodos(); 
-					$scope.todos=[];
-					 $scope.$watch('currentPage + numPerPage', function() {
-						    var begin = (($scope.currentPage - 1) * $scope.numPerPage)
-						    , end = begin + $scope.numPerPage;
-						    
-						    $scope.filteredTodos = $scope.todos.slice(begin, end);
-						  });
+					
 				});
 		
