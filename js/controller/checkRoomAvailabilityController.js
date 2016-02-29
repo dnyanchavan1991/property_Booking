7e9894ed9e3c6d4f9d49d70da7fe281bd0f7117e
snehalgaikwad <sgaikwad@agilesoftsol.com>
2016-02-29 18:14:49 +0530

    code for pagination is updated

--- js/controller/checkRoomAvailabilityController.js
+++ js/controller/checkRoomAvailabilityController.js
@@ -1,5 +1,5 @@
 angular
-		.module('checkRoomAvailabilityApp', [])
+		.module('checkRoomAvailabilityApp', ['angularUtils.directives.dirPagination'])
 		.controller(
 				'checkRoomAvailabilityController',
 				function($scope, $http) {
