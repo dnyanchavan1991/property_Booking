e2296db81766cdd9ebbc1ea7beb921ed62361ed4
vishuawate <vishu.awate@gmail.com>
2016-03-20 17:48:17 +0530

    added changes, which Shruti reverted today

--- application/views/contact.php
+++ application/views/contact.php
@@ -332,12 +332,15 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 									</div>-->
 									<div class="clearfix"></div>
 								</div>
-								<div class="comments-bot" dir-paginate="reviews_count in reviews|itemsPerPage:5">
+								<div class="comments-bot" dir-paginate="reviews_count in reviews|itemsPerPage:10">
 									<p style="white-space: pre-line;">{{reviews_count.review_text}}</p>
 									<div class="text-left" >
-										<span class="red-star" ng-repeat="r_cnt in strtoint(reviews_count.star_rating)">â˜…</span>
+										<span class="red-star" ng-repeat="r_cnt in strtoint(reviews_count.star_rating)">★</span>
 									</div>
-									<h4><span class="glyphicon glyphicon-minus" aria-hidden="true"></span> {{reviews_count.customer_name}} <p style="display:inline-block;">{{reviews_count.check_in}} - {{reviews_count.check_out}}</p></h4>
+									<h4><span class="glyphicon glyphicon-minus" aria-hidden="true"></span> 
+										{{reviews_count.customer_name}} <br/>
+									<p style="display:inline-block;">Visited property during {{reviews_count.check_in}} - {{reviews_count.check_out}}</p>
+									</h4>
 								</div>
 							</div>
 						</div>
