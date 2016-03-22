87d18a587675de2bd1d8aa206de710ffbf7cb250
shrutikharge <skharge@agilesoftsol.com>
2016-03-22 15:39:42 +0530

    added gloval function which creates form dynamically& submir page attribute to login controller

---
+++ js/global/global_functions.js
@@ -0,0 +1,26 @@
+public function  checkLogin(page){
+	var name = '';
+	if(name !='') {
+		return true;
+	} else {
+		alert("Please login to book the property...!");
+		var form = $(document.createElement('form'));
+		$(form).attr("action",login_url);
+		$(form).attr("method", "POST");
+
+		var input = $("<input>")
+		    .attr("type", "text")
+		    .attr("name", "page")
+		    .val(page );
+
+
+		$(form).append($(input));
+
+		form.appendTo( document.body )
+
+		$(form).submit();
+		return false;
+		
+	}
+}
+	
