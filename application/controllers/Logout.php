2cb715be80781d3689ba73692f80d3c24e800d8d
shrutikharge <skharge@agilesoftsol.com>
2016-03-23 18:44:38 +0530

    added code logout code

---
+++ application/controllers/Logout.php
@@ -0,0 +1,13 @@
+<?php
+class Logout extends CI_Controller {
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->library('session');
+	}
+	
+	public function index(){
+		session_destroy();
+				header("location:Index1");
+	}
+}
