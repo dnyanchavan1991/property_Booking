4143786e7c868d0ab9f46b1a00e976781479b40e
Operand Technologies & IT Solutions LLP <vishalgoneado@gmail.com>
2017-02-04 22:45:42 +0530

    basepath updated was left

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/original-dev-current/';
+$config['base_url'] = 'http://localhost/property_booking222/SVN/branches/dev/';
 
 /*
 |--------------------------------------------------------------------------
